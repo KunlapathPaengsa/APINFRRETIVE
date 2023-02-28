81a025041899e81b305879b80cc81025f9bf28aa
Kunlapath Paengsa <kunlapath.paengsa@gmail.com>
2023-03-01 00:34:07 +0700

    Add Configuration

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 using APINFRRETIVE.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.Services.AddDbContext<DataContext>();
+builder.Services.AddDbContext<DataContext>(options=> options.UseSqlServer(builder.Configuration.GetConnectionString("LocalConnection")));
 
 var app = builder.Build();
 app.UseHttpsRedirection();

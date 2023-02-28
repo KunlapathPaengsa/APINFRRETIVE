81f1849f09d7f380cf56bb251bca2cb8521f9b51
Kunlapath Paengsa <kunlapath.paengsa@gmail.com>
2023-02-28 23:47:05 +0700

    Add Model

---
+++ APINFRRETIVE.DAL/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace APINFRRETIVE.DAL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Character> Characters => Set<Character>();
+    }
+
+    public class Character
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [MaxLength(100)]
+        public string Description { get; set; }
+
+        public bool graduated { get; set; }
+    }
+}
